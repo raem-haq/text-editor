5eae59bf00894a15f834fbdc81335ebc100db4e6
Raem Haq <86297821+raem-haq@users.noreply.github.com>
2026-09-10 11:58:54 +0100

    Allow cursor to move (previously only exist at EOF)

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const textBox = document.querySelector("#text-box");
 var currentLine = document.querySelector("#line-1");
-
+/*
+const cursor = document.createElement('span');
+cursor.className = 'cursor';
+currentLine.appendChild(cursor);
+*/
 
 var allLines = [currentLine];
 var hasWritten = false;
 var currentLineNo = 0; // zero-indexed
 var cursorIndex = 0;
+var verticalMovement = false;
+var savedCursorIndex = cursorIndex;
 
 function removeAt(value, i) {   
     if (i < 0 || i >= value.length) {
@@ -52,6 +58,50 @@ function keyHandler(event){
                 currentLine.remove();
                 currentLine = newLine;
                 currentLineNo--;
+            }
+            break;
+        case "ArrowLeft":
+            if (cursorIndex > 0){
+                cursorIndex --;
+            } else {
+                currentLine = allLines[currentLineNo-1];
+                cursorIndex = newLine.textContent.length - 1;
+                currentLineNo --;
+            }
+            break;
+        
+        case "ArrowRight":
+            if (cursorIndex < currentLine.length - 1){
+                cursorIndex ++;
+            } else if (currentLineNo < allLines.length - 1) {
+                currentLine = allLines[currentLineNo+1];
+                cursorIndex = 0;
+                currentLineNo ++;
+            }
+            break;
+        case "ArrowUp":
+            if (currentLineNo > 0){
+                currentLine = allLines[currentLineNo-1];
+                if (verticalMovement){
+                    cursorIndex = Math.min(savedCursorIndex, currentLine.length - 1);
+                } else {
+                    verticalMovement = true;
+                    savedCursorIndex = cursorIndex;
+                }
+            } else {
+                cursorIndex = 0;
+            }
+            break;
+        case "ArrowDown":
+            if (currentLine < allLines.length - 1) {
+                currentLine = allLines[currentLineNo+1];
+                if (!verticalMovement) {
+                    verticalMovement = true;
+                    savedCursorIndex = cursorIndex;
+                }
+                cursorIndex = Math.min(savedCursorIndex, currentLine.length - 1);
+            } else {
+                cursorIndex = currentLine.length - 1
             }
             break;
         default:
